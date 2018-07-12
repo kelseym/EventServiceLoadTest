320fe7c4fc02f0c54eee4a8fdf1c50e1dc321fd4
Matt Kelsey <kelseym@mir.wustl.edu>
2018-07-12 16:27:42 -0500

    no message

--- src/TestRunner.groovy
+++ src/TestRunner.groovy
@@ -1,5 +1,8 @@
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
+import groovyx.net.http.ContentType
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.Method
 import org.apache.http.auth.AuthScope
 import org.apache.http.auth.UsernamePasswordCredentials
 import org.apache.http.client.CredentialsProvider
@@ -23,15 +26,8 @@ UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(USER, 
 provider.setCredentials(AuthScope.ANY, credentials)
 HttpClient client = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build()
 
-def map = [:]
-map["name"] = "Maritime DevCon"
-map["address"] = "Fredericton"
-map["handle"] = "maritimedevcon"
 
-def jsonBody = new JsonBuilder(map).toString()
-
-
-
+// Get all events
 def getEventsUrl = BASE_URL + 'events'
 URIBuilder builder = new URIBuilder(getEventsUrl);
 builder.setParameter("load-details", "true")
@@ -51,33 +47,72 @@ eventListing.each {event ->
 }
 
 
+// Get actions for each event type
+def getActionUrl = BASE_URL + 'actionsbyevent'
+eventListing.each {event ->
+    builder = new URIBuilder(getActionUrl)
+    builder.setParameter('event-type', event['type'])
+    getRequest = new HttpGet(builder.build());
+    getRequest.addHeader("content-type", "application/json")
+    response = client.execute(getRequest)
+    bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()))
+    jsonResponse = bufferedReader.getText()
+    slurper = new JsonSlurper()
+    actionListing = slurper.parseText(jsonResponse)
+    println("Actions for " + event['type'])
+    actionListing.each { action ->
+        println(action.toString())
+    }
+}
 
-def loggingActionForMRScansSubscription = "{\n" +
-        "    \"name\": \"MR Scan Archive on ABC123\",\n" +
-        "    \"active\": true,\n" +
-        "    \"action-key\": \"org.nrg.xnat.eventservice.actions.EventServiceLoggingAction:org.nrg.xnat.eventservice.actions.EventServiceLoggingAction\",\n" +
-        "    \"event-filter\": {\n" +
-        "        \"event-type\": \"org.nrg.xnat.eventservice.events.ScanEvent\",\n" +
-        "    \"project-ids\": [\n" +
-        "      \"ABC123\"\n" +
-        "    ],\n" +
-        "    \"status\": \"CREATED\",\n" +
-        "    \"json-path-filter\": \"\$[?(@.xsiType =~ /.*MRScanData/i && @.frames  > 100 && @.scanner-manufacturer =~ /siemens/i && @.scanner-model =~ /TRIOTIM/i )]\"\n" +
-        "    },\n" +
-        "    \"act-as-event-user\": false\n" +
+def loggingActionForMRScansSubscription = "{" +
+        "    \"name\": \"MR Scan Archive on ABC123\"" +
+        "    \"active\": true," +
+        "    \"action-key\": \"org.nrg.xnat.eventservice.actions.EventServiceLoggingAction:org.nrg.xnat.eventservice.actions.EventServiceLoggingAction\"," +
+        "    \"event-filter\": {" +
+        "        \"event-type\": \"org.nrg.xnat.eventservice.events.ScanEvent\"," +
+        "    \"project-ids\": [" +
+        "      \"ABC123\"" +
+        "    ]," +
+        "    \"status\": \"CREATED\"," +
+        "    \"json-path-filter\": \"\$[?(@.xsiType =~ /.*MRScanData/i && @.frames  > 100 && @.scanner-manufacturer =~ /siemens/i && @.scanner-model =~ /TRIOTIM/i )]\"" +
+        "    }," +
+        "    \"act-as-event-user\": false" +
         "}"
 
-def loggingActionForMRSessionSubscription = "{\n" +
-        "    \"name\": \"MR Scan Archive on ABC123\",\n" +
-        "    \"active\": true,\n" +
-        "    \"action-key\": \"org.nrg.xnat.eventservice.actions.EventServiceLoggingAction:org.nrg.xnat.eventservice.actions.EventServiceLoggingAction\",\n" +
-        "    \"event-filter\": {\n" +
-        "        \"event-type\": \"org.nrg.xnat.eventservice.events.ScanEvent\",\n" +
-        "    \"project-ids\": [\n" +
-        "      \"ABC123\"\n" +
-        "    ],\n" +
-        "    \"status\": \"CREATED\",\n" +
-        "    \"json-path-filter\": \"\$[?(@.xsiType =~ /.*MRScanData/i && @.frames  > 100 && @.scanner-manufacturer =~ /siemens/i && @.scanner-model =~ /TRIOTIM/i )]\"\n" +
-        "    },\n" +
-        "    \"act-as-event-user\": false\n" +
+def loggingActionForMRSessionSubscription = "{" +
+        "    \"name\": \"MR Scan Archive on ABC123\"," +
+        "    \"active\": true," +
+        "    \"action-key\": \"org.nrg.xnat.eventservice.actions.EventServiceLoggingAction:org.nrg.xnat.eventservice.actions.EventServiceLoggingAction\"," +
+        "    \"event-filter\": {" +
+        "        \"event-type\": \"org.nrg.xnat.eventservice.events.ScanEvent\"," +
+        "    \"project-ids\": [" +
+        "      \"ABC123\"" +
+        "    ]," +
+        "    \"status\": \"CREATED\"," +
+        "    \"json-path-filter\": \"\$[?(@.xsiType =~ /.*MRScanData/i && @.frames  > 100 && @.scanner-manufacturer =~ /siemens/i && @.scanner-model =~ /TRIOTIM/i )]\"" +
+        "    }," +
+        "    \"act-as-event-user\": false" +
         "}"
+
+
+def createSubscriptionBuilder = new HTTPBuilder(BASE_URL)
+def newItemId
+createSubscriptionBuilder.request(Method.POST, ContentType.JSON){
+    req->
+        req.uri.path = 'subscription'
+        req.uri.query = [format:'json']
+        req.body = loggingActionForMRScansSubscription
+
+        req.response.success { resp, json->
+            assert resp.status == 201
+            newItemId = json.id
+        }
+
+        req.response.failure = { resp ->
+            throw new Exception("Failed to create new subscription at " + req.uri.toString())
+        }
+}
+
+println "New subscription ID = " + newItemId.toString()
+
